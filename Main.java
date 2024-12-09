4353bfbf1d2590dab27c304f8dbd4d2234d58612
unknown <rockanil2918@gmail.com>
2024-12-09 11:15:44 +0530

    First

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public class void main(string[] args){
+        system.out.main("hello world");
+        system.out.println(2);
+        system.out.println("3+3");
+        system.out.name="anil";
+        system.out.main(name);
+    }
+}
